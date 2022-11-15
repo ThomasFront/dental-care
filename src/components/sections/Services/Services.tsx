66aa15989c946f57c62c1de184a3fc747923d1a9
ThomasFront <cyzmenpl@gmail.com>
2022-11-15 13:18:15 +0100

    added disabled hours

--- src/components/sections/Services/Services.tsx
+++ src/components/sections/Services/Services.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import { services } from '../../../utils'
 import { SectionTitle } from '../../SectionTitle'
 import { ServiceCard } from '../../ServiceCard'
 import { TextWrapper } from '../../TextWrapper'
 import { ContentContainer, GrayFade, ServicesContainer, Wrapper } from './Services.styles'
 
+type StrapiArrayType<T> = {
+  data: Array<{
+    id: number,
+    attributes: {
+      createdAt: string,
+      publishedAt: string,
+      updatedAt: string
+    } & T
+  }>
+}
+
+type ServicesType = StrapiArrayType<{
+  Description: string,
+  Title: string,
+}>
+
 export const Services = () => {
+  const getServices = () => {
+    axios.get('http://localhost:1337/api/services').then(response => {
+      console.log(response);
+    });
+  }
+
+  useEffect(() => {
+    getServices()
+  }, [])
+
   return (
     <div id='services'>
       <Wrapper>
